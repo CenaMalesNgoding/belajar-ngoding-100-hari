fcadba8f22191dfa58e2ef324579f00e9d51170b
Avicena azra al ard bakron <avicenaazra82@gmail.com>
2025-06-13 05:26:17 +0700

    day 5: cari nilai terbesar dari dua angka

---
+++ day 5.py
@@ -0,0 +1,9 @@
+a = int(input("Masukkan angka pertama: "))
+b = int(input("Masukkan angka kedua: "))
+
+if a > b:
+    print("Angka terbesar adalah:", a)
+elif b > a:
+    print("Angka terbesar adalah:", b)
+else:
+    print("Kedua angka sama besar.")
