863666ad5a503244b388ec2869214c345fb82b81
Avicena azra al ard bakron <avicenaazra82@gmail.com>
2025-06-13 05:30:58 +0700

    day 10: fungsi penjumlahan dua angka

---
+++ day 10.py
@@ -0,0 +1,8 @@
+def jumlahkan(a, b):
+    return a + b
+
+x = int(input("Masukkan angka pertama: "))
+y = int(input("Masukkan angka kedua: "))
+
+hasil = jumlahkan(x, y)
+print("Hasil penjumlahan:", hasil)
